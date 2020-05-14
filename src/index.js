79f3b9635c0d27d7f131c19344603e71bc59b51f
wasabhi <abhinav.keswani@trineo.co.nz>
2020-05-13 21:45:30 -0600

    Base React App

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
